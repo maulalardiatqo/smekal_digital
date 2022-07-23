8dd322d3028af08e5271610cfb9c070b819a2f21
ardi <maulalardiatqo123@gmail.com>
2022-07-23 12:34:51 +0700

    sidebar

--- application/views/template_admin/sidebar.php
+++ application/views/template_admin/sidebar.php
@@ -34,27 +34,10 @@
                       <span class="nav-text">Inventaris</span>
                   </a>
                   <ul aria-expanded="false">
-                      <li><a href="app-profile.html">Profile</a></li>
-                      <li><a href="post-details.html">Post Details</a></li>
-                      <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Email</a>
-                          <ul aria-expanded="false">
-                              <li><a href="email-compose.html">Compose</a></li>
-                              <li><a href="email-inbox.html">Inbox</a></li>
-                              <li><a href="email-read.html">Read</a></li>
-                          </ul>
-                      </li>
-                      <li><a href="app-calender.html">Calendar</a></li>
-                      <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Shop</a>
-                          <ul aria-expanded="false">
-                              <li><a href="ecom-product-grid.html">Product Grid</a></li>
-                              <li><a href="ecom-product-list.html">Product List</a></li>
-                              <li><a href="ecom-product-detail.html">Product Details</a></li>
-                              <li><a href="ecom-product-order.html">Order</a></li>
-                              <li><a href="ecom-checkout.html">Checkout</a></li>
-                              <li><a href="ecom-invoice.html">Invoice</a></li>
-                              <li><a href="ecom-customers.html">Customers</a></li>
-                          </ul>
-                      </li>
+                      <li><a href="app-profile.html">Kelas</a></li>
+                      <li><a href="post-details.html">Lab Komputer</a></li>
+                      <li><a href="post-details.html">Perbankan</a></li>
+                      <li><a href="post-details.html">Bengkel</a></li>
                   </ul>
               </li>
               <li><a href="<?= base_url('admin/guru') ?>" aria-expanded=" false">
