ecaf9e0eb41779fbd9b8186d250367f80c359987
MaulalArdiAtqo <maulalardiatqo123@gmail.com>
2022-07-28 21:31:47 +0700

    push prodi

---
+++ db/smekal_prodi.sql
@@ -0,0 +1,19 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prodi`
+--
+
+CREATE TABLE `prodi` (
+  `id_prodi` int(11) NOT NULL,
+  `nama_prodi` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `prodi`
+--
+
+INSERT INTO `prodi` (`id_prodi`, `nama_prodi`) VALUES
+(1, 'TKJ'),
+(2, 'TKR'),
+(3, 'A-PKM');
