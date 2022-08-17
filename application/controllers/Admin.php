e53c400fd1895b6f5e425aaaf076bf1bbe628248
rizalazky <rizalazky26@gmail.com>
2022-08-17 21:34:15 +0700

    update tagihan

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -507,6 +507,7 @@ class Admin extends CI_Controller
                 $isAlreadyExist = $this->db->get_where('tagihan_detail',['to'=>$dataInsert['to'],'id_tagihan'=>$this->input->post('id_tagihan')])->num_rows();
                 if($isAlreadyExist == 0){
                     $this->db->insert('tagihan_detail',$dataInsert);
+                    // Insert to pemasukan
                 }
              }
         }
